4ea469f3aee3967b0148b7627911e8b2c7d24645
SerOne <vladimir366940@gmail.com>
2026-04-25 13:30:45 +0300

    Добавьте файлы проекта.

---
+++ Progekt_methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Power(2, 3));  
+        Console.WriteLine(Power(5, 2));  
+        Console.WriteLine(Power(3, 4));  
+        Console.WriteLine(Power(2, 0));  
+        Console.WriteLine(Power(4, -2)); 
+    }
+
+    static double Power(double baseNumber, int exponent)
+    {
+        if (exponent == 0)
+            return 1;
+
+        bool isNegativeExponent = exponent < 0;
+
+        int absExponent = Math.Abs(exponent);
+
+        double result = 1;
+
+        for (int i = 0; i < absExponent; i++)
+        {
+            result *= baseNumber;
+        }
+
+        if (isNegativeExponent)
+            result = 1 / result;
+
+        return result;
+    }
+}
